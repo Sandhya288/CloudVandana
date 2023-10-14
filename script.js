d34a985a18efc7269ec10ca99ea2b9eed572e23a
Sandhya288 <91210210+Sandhya288@users.noreply.github.com>
2023-10-14 08:57:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('survey-form');
-    const popup = document.getElementById('popup');
-    const closeButton = document.getElementById('close-popup');
-    const summaryContainer = document.getElementById('summary');
+// Get references to form elements and the popup
+const form = document.getElementById('survey-form');
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('close-popup');
+const summary = document.getElementById('summary');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const summary = [];
-        for (const [key, value] of formData.entries()) {
-            summary.push(`<strong>${key}:</strong> ${value}`);
-        }
-        summaryContainer.innerHTML = summary.join('<br>');
-        popup.style.display = 'block';
-    });
+// Add a submit event listener to the form
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    displayPopup();
+});
 
-    closeButton.addEventListener('click', function() {
-        popup.style.display = 'none';
-        form.reset();
-    });
+// Function to display the popup
+function displayPopup() {
+    // Get values from form fields
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const dob = document.getElementById('date-of-birth').value;
+    const country = document.getElementById('country').value;
 
-    form.addEventListener('reset', function() {
-        summaryContainer.innerHTML = '';
-    });
+    // Get selected gender values
+    const genderElements = document.querySelectorAll('input[name="gender"]:checked');
+    const genderValues = Array.from(genderElements).map(g => g.value);
+
+    const profession = document.getElementById('profession').value;
+    const email = document.getElementById('email').value;
+    const mobileNumber = document.getElementById('mobile-number').value;
+
+    // Create a summary message with the collected values
+    const summaryMessage = `
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Date of Birth:</strong> ${dob}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Gender:</strong> ${genderValues.join(', ')}</p>
+        <p><strong>Profession:</strong> ${profession}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mobile Number:</strong> ${mobileNumber}</p>
+    `;
+
+    // Display the summary message in the popup
+    summary.innerHTML = summaryMessage;
+
+    // Show the popup
+    popup.style.display = 'block';
+}
+
+// Close the popup when the close button is clicked
+closePopup.addEventListener('click', function() {
+    popup.style.display = 'none';
+});
+
+// Reset the form when the popup is closed
+closePopup.addEventListener('click', function() {
+    form.reset();
 });
